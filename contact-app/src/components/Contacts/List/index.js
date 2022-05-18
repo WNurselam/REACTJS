a7ac5d6b600df1fdd22d3656f9125ea86fa73331
Nurselam <77880259+WNurselam@users.noreply.github.com>
2022-05-18 19:10:42 +0300

    create project

---
+++ contact-app/src/components/Contacts/List/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function List() {
+    return (
+        <div>
+            List
+            
+        </div>
+    )
+}
+
+export default List;
---
+++ contact-app/src/components/Contacts/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import List from './List'
+
+
+export function Contact() {
+    return (
+        <div>
+            <List/>
+            
+            
+        </div>
+    )
+}
+export default Contact
